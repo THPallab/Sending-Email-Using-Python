7b3b312dfcfeae665892517cf1d183a683abd4cf
THPallab <tanvir.h.pallab@gmail.com>
2020-02-18 15:04:01 +0600

    First Commit

---
+++ Email.py
@@ -0,0 +1,18 @@
+import smtplib, ssl
+
+port = 465
+smtp_server = "smtp.gmail.com"
+sender_email = "nainam549@gmail.com"
+receiver_email = "th4pallab@gmail.com"
+password = "amarkononamnai1"
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
+
+print ('Successful')
